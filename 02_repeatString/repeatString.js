f363ec4704f3c049ebfe20b1f68c32f9d1b6e24d
Sam <sam@example.com>
2022-08-07 22:00:27 +0100

    bring on 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(phrase, numTimes) {
+    let phraseCollection = [];
 
+    if(numTimes < 0){
+     return 'ERROR';
+    } 
+    
+    for(i=0; i < numTimes; i++ ) {
+        phraseCollection.push(phrase)
+    }
+    return phraseCollection.join('')
 };
 
 // Do not edit below this line
