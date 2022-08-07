462d9a759efe336d28684a1f4931d8e6b0ea4723
Sam <sam@example.com>
2022-08-07 22:31:27 +0100

    Reversed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(stringForwards) {
+    let stringForwardsArray = stringForwards.split("");
+    
+    let stringReverse = []
+    for(let i = 0; i < stringForwards.length; i++ ) {
+        stringReverse.push(stringForwardsArray.pop())
+    }
+    return stringReverse.join('')
 };
 
 // Do not edit below this line
