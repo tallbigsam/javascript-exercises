d2060534bd47a090a7fab8ee30aaea24338af033
Sam Redman <sam.redman@m-m433n72d9r.home>
2022-09-05 21:48:18 +0100

    done 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arrayNumbers, ...args) {
+    
+    args.forEach((arg) => {
+        const indexOf = arrayNumbers.indexOf(arg)
+        if (indexOf > -1) {
+            arrayNumbers.splice(indexOf, 1)
+        }    
+    })
+    return arrayNumbers;
 };
 
 // Do not edit below this line
